88cce519ce06ee62e802677ee870a057a4b4e1bb
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-07-26 12:25:32 +0800

    Add cucumber report options

--- microservices/productservice/src/main/java/com/ib/it/productservice/tests/RunCucumberTest.java
+++ microservices/productservice/src/main/java/com/ib/it/productservice/tests/RunCucumberTest.java
@@ -7,7 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "microservices/productservice/src/main/resources/features",
-        glue = "com.ib.it.productservice.tests.stepdefinitions"
+        glue = "com.ib.it.productservice.tests.stepdefinitions",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/html-report.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "junit:target/cucumber-reports/cucumber.xml"
+        },
+        monochrome = true
+
 )
 public final class RunCucumberTest {
     public RunCucumberTest() {
